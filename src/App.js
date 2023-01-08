b241925dafd04dc9d2a21bd2aacf51ec65f6f834
Luisito <luisitoylag@gmail.com>
2023-01-08 16:16:51 -0500

    Creación del Proyecto

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Index from "./pages";
+import { About } from "./pages/About";
+import { Contact } from "./pages/Contact";
+import View from "./pages/view";
+import { Navbar } from "./components/Navbar";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/index" element={<Index />} />
+        <Route path="/view" element={<View />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
